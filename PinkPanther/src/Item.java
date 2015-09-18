e08dcca6154f5ed0374f65cf134421b48e726376
gaobo9109 <gaobo9109@gmail.com>
2015-09-18 22:44:43 +0800

    added Item class

--- PinkPanther/src/Item.java
+++ PinkPanther/src/Item.java
@@ -1,4 +1,26 @@
 
 public class Item {
-
+	String name;
+	boolean isDone;
+	
+	public Item(String name){
+		this.name=name;
+		isDone=false;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public void markAsDone(){
+		isDone=true;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public boolean isFinished(){
+		return isDone;
+	}
 }
