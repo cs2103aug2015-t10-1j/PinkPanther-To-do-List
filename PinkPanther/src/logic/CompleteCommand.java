482790c7fb6f382c24b3366c4375815ad3e4a25d
Brayton Goh <braytongwm93@gmail.com>
2015-09-21 23:46:33 +0800

    Wrote CompleteCommand.java for Logic.

--- PinkPanther/src/logic/CompleteCommand.java
+++ PinkPanther/src/logic/CompleteCommand.java
@@ -1,17 +1,35 @@
 package logic;
 
+import common.Event;
+import common.Task;
+
 public class CompleteCommand implements Command {
-
-	@Override
+	private Task taskRef;
+	private boolean previousState;
+	
+	public CompleteCommand () {
+	}
+	
+	public String execute(Task task, boolean isComplete) {
+		previousState = task.getCompletionStatus();
+		
+		if (isComplete) {
+			task.setCompleteState(true);
+			taskRef = task;
+			return task.getName() + " set to 'Complete'.";
+		}
+		else {
+			task.setCompleteState(false);
+			taskRef = task;
+			return task.getName() + " set to 'Incomplete'.";
+		}
+	}
+	
 	public void undo() {
-		// TODO Auto-generated method stub
-
+		this.execute(taskRef, previousState);
 	}
 
-	@Override
 	public void redo() {
-		// TODO Auto-generated method stub
-
+		this.execute(taskRef, previousState);
 	}
-
 }
