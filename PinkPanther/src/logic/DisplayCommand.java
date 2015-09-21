2a160bf6a2f8962bf4f46a52f4dfde5b076e6f6a
Brayton Goh <braytongwm93@gmail.com>
2015-09-21 19:24:53 +0800

    Added packageEventsWithSameDate() to DisplayCommand.java.

--- PinkPanther/src/logic/DisplayCommand.java
+++ PinkPanther/src/logic/DisplayCommand.java
@@ -14,9 +14,9 @@ public class DisplayCommand {
 		this.handler = handler;
 	}
 	
-	public ArrayList<TaskHandler> executeDatedTask() {
+	public ArrayList<ArrayList<Event>> executeDateLineTask() {
 		ArrayList<Event> dateLineList = this.handler.getDateLineList();
-		ArrayList<TaskHandler> overallContainer;
+		ArrayList<ArrayList<Event>> overallContainer;
 		
 		ArrayList<LocalDate> dates = obtainDates(dateLineList); //Get the various dates in eventList.
 		
@@ -24,18 +24,30 @@ public class DisplayCommand {
 		int numOfDates = dates.size();
 		
 		for (int i = 0; i < numOfDates; i++) {
-			overallContainer.add(packageEventsWithSameDate(dates.get(i)));
+			overallContainer.add(packageEventsWithSameDate(dates.get(i), dateLineList));
 		}
 		
 		return overallContainer;
 	}
 
-	private static ArrayList<LocalDate> obtainDates(ArrayList<Event> eventList) {
+	private ArrayList<Event> packageEventsWithSameDate(LocalDate currentDate, ArrayList<Event> dateLineList) {
+		ArrayList<Event> packagedList = new ArrayList<Event>();
+		
+		for (Event event: dateLineList) {
+			if (event.getDate().equals(currentDate)) {
+				packagedList.add(event);
+			}
+		}
+		
+		return packagedList;
+	}
+
+	protected static ArrayList<LocalDate> obtainDates(ArrayList<Event> eventList) {
 		ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
 		int eventListSize = eventList.size();
 		
 		for (int i = 0; i < eventListSize; i++) {
-			LocalDate currentDate= eventList.get(i).getDate();
+			LocalDate currentDate = eventList.get(i).getDate();
 			
 			if (!hasDate(dates, currentDate)) {
 				dates.add(eventList.get(i).getDate());
