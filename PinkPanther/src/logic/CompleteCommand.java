7233ac7c3172092bd83f26b8a6e73b9e1a5851a1
Brayton Goh <braytongwm93@gmail.com>
2015-09-21 21:37:25 +0800

    Wrote CompleteCommand.java Constructor.

---
+++ PinkPanther/src/logic/CompleteCommand.java
@@ -0,0 +1,17 @@
+package logic;
+
+public class CompleteCommand implements Command {
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void redo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
