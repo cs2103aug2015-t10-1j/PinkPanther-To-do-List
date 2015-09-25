36350f72ed19fb006208ca4d0b81e6136a0ab397
gaobo9109 <gaobo9109@gmail.com>
2015-09-25 12:43:07 +0800

    add custom comparator to sort dated tasks without time

--- PinkPanther/src/common/Task.java
+++ PinkPanther/src/common/Task.java
@@ -129,4 +129,5 @@ public class Task {
 		return status;
 	}
 	
+	
 }
--- PinkPanther/src/logic/TaskHandler.java
+++ PinkPanther/src/logic/TaskHandler.java
@@ -2,6 +2,7 @@ package logic;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.TreeMap;
 
 import storage.TaskStorage;
@@ -29,7 +30,23 @@ public class TaskHandler {
 	
 	private void sortTodoList(LocalDate date){
 		ArrayList<Task>tasks=todoList.get(date);
-		Collections.sort(tasks,(Task t1,Task t2)->t1.getTime().compareTo(t2.getTime()));
+		Collections.sort(tasks,new Comparator<Task>(){
+			public int compare(Task t1,Task t2){
+				if(t1.getTime()!=null && t2.getTime()!=null){
+					return t1.getTime().compareTo(t2.getTime());
+				}
+				else if(t1.getTime()==null && t2.getTime()!=null){
+					return 1;
+				}
+				else if(t1.getTime()!=null && t2.getTime()==null){
+					return -1;
+				}
+				else{
+					return t1.getName().compareTo(t2.getName());
+				}
+			}
+		});
+		
 		Collections.sort(tasks,(Task t1,Task t2)->t1.getTaskType().compareTo(t2.getTaskType()));
 	}
 	
@@ -110,6 +127,19 @@ public class TaskHandler {
 			}
 		}
 		return false;
+	}
+	
+	public void clearAllTasks(){
+		todoList.clear();
+		floatingList.clear();
+	}
+	
+	//for debugging purpose
+	public void printTodoAtDate(LocalDate date){
+		ArrayList<Task>tasks=todoList.get(date);
+		for(Task task:tasks){
+			System.out.println(task.getName());
+		}
 	}
 	
 	
