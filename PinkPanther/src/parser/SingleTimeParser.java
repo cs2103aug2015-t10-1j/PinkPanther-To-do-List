e2ab6f04ab12d22973cc97e0dcd6263254d58e81
CS <rieru89@gmail.com>
2015-09-29 01:28:00 +0800

    Added a list of words that can indicate time

--- PinkPanther/src/parser/SingleTimeParser.java
+++ PinkPanther/src/parser/SingleTimeParser.java
@@ -2,6 +2,7 @@ package parser;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,7 +13,9 @@ public class SingleTimeParser {
 	private static final List<String> TIME_FORMATS= 
 			Collections.unmodifiableList(Arrays.asList("h:mma", "hh:mma", 
 					"h.mma", "hh.mma", "hmma", "hhmma", "H:mm", "HH:mm", 
-					"H.mm", "HH.mm", "Hmm", "Hhmm", "ha", "hha"));
+					"H.mm", "HH.mm", "Hmm", "HHmm", "ha", "hha"));
+	private static final String[] TIME_INDICATORS = {"TONIGHT", "NOW", "MORNING",
+			"AFTERNOON", "EVENING", "NIGHT"};
 	
 	private static ArrayList<String> validTimeFormats;
 	
@@ -23,9 +26,16 @@ public class SingleTimeParser {
 
 	public LocalTime parse(String time) {
 		
-		// if time contains a certain keyword, refer to a list and parse separately
-		
 		time = time.toUpperCase();
+		
+		// if time contains a certain keyword, refer to a list and parse separately
+		for (int i = 0; i < TIME_INDICATORS.length; i++) {
+			if (time.equals(TIME_INDICATORS[i])) {
+				return parseTimeWord(time);
+			}
+		}
+		
+		// otherwise parse it as a numerical time
 		for (String timeFormat : validTimeFormats) {
 			LocalTime parsedTime = compareTimeFormat(time, timeFormat);
 			if (parsedTime != null) {
@@ -45,6 +55,22 @@ public class SingleTimeParser {
 			
 		} catch (Exception e) {
 			// some exception
+		}
+		return null;
+	}
+	
+	private LocalTime parseTimeWord (String time) {
+		switch (time) {
+			case "NOW":
+				return LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+			case "TONIGHT":
+			case "NIGHT":
+				return LocalTime.of(19,0);
+			case "MORNING":
+				return LocalTime.of(9,0);
+			case "AFTERNOON":
+				return LocalTime.of(12,0);
+			default:
 		}
 		return null;
 	}
