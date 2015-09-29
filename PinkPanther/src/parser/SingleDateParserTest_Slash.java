93e8bfc609d0562d06675a426b9bbdeb541809f4
Brayton Goh <Braytongwm93@gmail.com>
2015-09-29 22:29:54 +0800

    Added extra test cases to SingleDateparserTest to cater to CS's newly added 'full month name' formats.

--- PinkPanther/src/parser/SingleDateParserTest_Slash.java
+++ PinkPanther/src/parser/SingleDateParserTest_Slash.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 
 public class SingleDateParserTest_Slash {
 
-	//@Test
+	@Test
 	public void test_Parse_SlashCase1() { // dd/MM/yy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -37,12 +37,12 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase2() { // d/MM/yy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 9; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -66,12 +66,12 @@ public class SingleDateParserTest_Slash {
 			}
 		}
 	}
-	//@Test
+	@Test
 	public void test_Parse_SlashCase3() { // dd/M/yy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -92,12 +92,12 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase4() { // d/M/yy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + "/" + Integer.toString(month) +
@@ -107,13 +107,13 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase5() { // dd/MMM/yy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
@@ -134,13 +134,13 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase6() { // d/MMM/yy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + "/" + month[j - 1] +
@@ -150,12 +150,12 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase7() { // dd/MM/yyyy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -180,12 +180,12 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase8() { // d/MM/yyyy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 9; day++) {			
 					if (month < 10){
@@ -201,12 +201,12 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase9() { // dd/M/yyyy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -226,12 +226,12 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase10() { // d/M/yyyy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + "/" + Integer.toString(month) +
@@ -241,13 +241,13 @@ public class SingleDateParserTest_Slash {
 		}
 	}
 	
-	//@Test
+	@Test
 	public void test_Parse_SlashCase11() { // dd/MMM/yyyy
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
@@ -273,11 +273,96 @@ public class SingleDateParserTest_Slash {
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + "/" + month[j - 1] +
 								"/" + Integer.toString(year)), LocalDate.of(year, j, day));	
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SlashCase13() { // d/MMMM/yyyy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 9; day++) {
+					assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+							"-" + Integer.toString(year)), LocalDate.of(year, j, day));
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SlashCase14() { // dd/MMMM/yyyy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 31; day++) {
+					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
+							|| (day > 28 && j == 2)) {
+						continue;
+					}
+					else if (day < 10) {
+						assertEquals(dateParser.parse("0" + Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year)), LocalDate.of(year, j, day));
+					}
+					else {
+						assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year)), LocalDate.of(year, j, day));
+					}	
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SlashCase15() { // dd/MMMM/yy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 31; day++) {
+					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
+							|| (day > 28 && j == 2)) {
+						continue;
+					}
+
+					else if (day < 10) {
+						assertEquals(dateParser.parse("0" + Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year).substring(2)), LocalDate.of(year, j, day));
+					}
+					else {
+						assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year).substring(2)), LocalDate.of(year, j, day));
+					}	
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SlashCase16() { // d/MMMM/yy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 9; day++) {
+					assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+							"-" + Integer.toString(year).substring(2)), LocalDate.of(year, j, day));	
 				}
 			}
 		}
--- PinkPanther/src/parser/SingleDateParserTest_Space.java
+++ PinkPanther/src/parser/SingleDateParserTest_Space.java
@@ -12,7 +12,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -42,7 +42,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 9; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -71,7 +71,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -97,7 +97,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + " " + Integer.toString(month) +
@@ -113,7 +113,7 @@ public class SingleDateParserTest_Space {
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
@@ -140,7 +140,7 @@ public class SingleDateParserTest_Space {
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + " " + month[j - 1] +
@@ -155,7 +155,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -185,7 +185,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 		
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 12; month++) {
 				for (day = 1; day <= 9; day++) {			
 					if (month < 10){
@@ -206,7 +206,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && month == 4) || (day == 31 && month == 6) || (day == 31 && month == 9) || (day == 31 && month == 11)
@@ -231,7 +231,7 @@ public class SingleDateParserTest_Space {
 		SingleDateParser dateParser = new SingleDateParser();
 		int day, month, year;
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (month = 1; month <= 9; month++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + " " + Integer.toString(month) +
@@ -247,7 +247,7 @@ public class SingleDateParserTest_Space {
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 31; day++) {
 					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
@@ -273,11 +273,96 @@ public class SingleDateParserTest_Space {
 		int day, year;
 		String[] month = {"Jan", "feb", "MAR", "ApR", "mAy", "juN", "JUl", "auG", "Sep", "OcT", "nOv", "DEC"};
 
-		for (year = 2015; year <= 2099; year++) {
+		for (year = 2016; year <= 2099; year++) {
 			for (int j = 1; j <= month.length; j++) {
 				for (day = 1; day <= 9; day++) {
 					assertEquals(dateParser.parse(Integer.toString(day) + " " + month[j - 1] +
 								" " + Integer.toString(year)), LocalDate.of(year, j, day));	
+				}
+			}
+		}
+	}
+	
+	//@Test
+	public void test_Parse_SpaceCase13() { // d MMMM yyyy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 9; day++) {
+					assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+							"-" + Integer.toString(year)), LocalDate.of(year, j, day));
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SpaceCase14() { // dd MMMM yyyy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 31; day++) {
+					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
+							|| (day > 28 && j == 2)) {
+						continue;
+					}
+					else if (day < 10) {
+						assertEquals(dateParser.parse("0" + Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year)), LocalDate.of(year, j, day));
+					}
+					else {
+						assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year)), LocalDate.of(year, j, day));
+					}	
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SpaceCase15() { // dd MMMM yy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 31; day++) {
+					if ((day == 31 && j == 4) || (day == 31 && j == 6) || (day == 31 && j == 9) || (day == 31 && j == 11)
+							|| (day > 28 && j == 2)) {
+						continue;
+					}
+
+					else if (day < 10) {
+						assertEquals(dateParser.parse("0" + Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year).substring(2)), LocalDate.of(year, j, day));
+					}
+					else {
+						assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+								"-" + Integer.toString(year).substring(2)), LocalDate.of(year, j, day));
+					}	
+				}
+			}
+		}
+	}
+
+	@Test
+	public void test_Parse_SpaceCase16() { // d MMMM yy
+		SingleDateParser dateParser = new SingleDateParser();
+		int day, year;
+		String[] month = {"January", "february", "MARCH", "ApRiL", "mAy", "juNe", "JUly", "auGUST", "SepTemBER", "OcTobeR", "nOvEMBEr", "DECembeR"};
+
+		for (year = 2016; year <= 2099; year++) {
+			for (int j = 1; j <= month.length; j++) {
+				for (day = 1; day <= 9; day++) {
+					assertEquals(dateParser.parse(Integer.toString(day) + "-" + month[j - 1] +
+							"-" + Integer.toString(year).substring(2)), LocalDate.of(year, j, day));	
 				}
 			}
 		}
