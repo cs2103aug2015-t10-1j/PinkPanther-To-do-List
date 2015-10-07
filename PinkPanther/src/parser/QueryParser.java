70e4aac26ae25a2924fe2d6d199cf8835a755cc4
CS <rieru89@gmail.com>
2015-10-07 22:50:36 +0800

    Now returns a pair consisting of a LocalDate and an ArrayList of integers

--- PinkPanther/src/parser/QueryParser.java
+++ PinkPanther/src/parser/QueryParser.java
@@ -1,41 +1,98 @@
 package parser;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import common.Pair;
 import logic.Display;
 
 public class QueryParser implements Parser {
 	
-	private static final int INDEX_DATE = 0;
+	private static final int INDEX_KEYWORD = 0;
 	private static final int INDEX_INDEX = 1;
+	
+	public static void main(String[] args) {
+		QueryParser qp = new QueryParser();
+		Pair hello = qp.parse("3 jul, , 3, 4, 5");
+		System.out.println(hello.getFirst());
+		System.out.println(hello.getSecond().toString());
+		
+	}
 	
 	public Pair parse(String commandContent) {
 		String[] userInfo = commandContent.split(",");
 		userInfo = Auxiliary.trimStringArray(userInfo);
+		ArrayList<Integer> indexList = new ArrayList<Integer>();
 		SingleDateParser sdp = new SingleDateParser();
 		
-		Integer index = null;
-		try {
-			index = (Integer)Integer.parseInt(userInfo[INDEX_INDEX]);
-		} catch (Exception e) {
-			Display.setFeedBack("invalid int");
-			//Display some invalid input
-			return null;
-		} finally {
+		// retrieving multiple floating tasks
+		if (userInfo[INDEX_KEYWORD].equalsIgnoreCase("float")) {
+			for (int i = INDEX_INDEX; i < userInfo.length; i++) {
+				if (userInfo[i].isEmpty()) {
+					continue;
+				}
+				
+				try {
+					indexList.add(Integer.valueOf(userInfo[i]));
+				} catch (Exception e) {
+					Display.setFeedBack("Invalid index entered");
+					return null;
+				}
+			}
+			return new Pair<LocalDate, ArrayList<Integer>>(null, indexList);
 			
+		} 
+		
+		// retrieving multiple dated tasks
+		LocalDate date = sdp.parse(userInfo[INDEX_KEYWORD]);
+		if (date != null) {
+			// retrieve all tasks
+			if (userInfo.length == 2 && userInfo[INDEX_INDEX].equalsIgnoreCase("all")) {
+				return new Pair<LocalDate, ArrayList<Integer>>(date, null);
+			}
+			// retrieve multiple tasks
+			for (int i = INDEX_INDEX; i < userInfo.length; i++) {
+				if (userInfo[i].isEmpty()) {
+					continue;
+				}
+				try {
+					indexList.add(Integer.valueOf(userInfo[i]));
+				} catch (Exception e) {
+					Display.setFeedBack("Invalid index entered");
+					return null;
+				}
+			}
+			return new Pair<LocalDate, ArrayList<Integer>>(date, indexList);
 		}
 		
-		if (userInfo[INDEX_DATE].equalsIgnoreCase("float")) {
-			Pair<LocalDate, Integer> floatIndex = new Pair<LocalDate, Integer>(null,index);
-			return floatIndex;
-		}
+		// no keywords found!
+		Display.setFeedBack("No valid keyword or date entered");
+		return null;
 		
-		LocalDate date = sdp.parse(userInfo[INDEX_DATE]);
-		if (date == null) {
-			Display.setFeedBack("invalid date or float indicator");
-			return null;
-		}
-		Pair<LocalDate, Integer> dateIndex = new Pair<LocalDate, Integer>(date, index);
-		return dateIndex;
+		
+		
 	}
 }
+	
+	/*Integer index = null;
+	try {
+		index = (Integer)Integer.parseInt(userInfo[INDEX_INDEX]);
+	} catch (Exception e) {
+		Display.setFeedBack("invalid int");
+		//Display some invalid input
+		return null;
+	} finally {
+		
+	}
+	
+	if (userInfo[INDEX_DATE].equalsIgnoreCase("float")) {
+		Pair<LocalDate, Integer> floatIndex = new Pair<LocalDate, Integer>(null,index);
+		return floatIndex;
+	}
+	
+	LocalDate date = sdp.parse(userInfo[INDEX_DATE]);
+	if (date == null) {
+		Display.setFeedBack("invalid date or float indicator");
+		return null;
+	}
+	Pair<LocalDate, Integer> dateIndex = new Pair<LocalDate, Integer>(date, index);
+	return dateIndex;*/
