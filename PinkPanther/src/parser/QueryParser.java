22aec7643fdc12350f6cf37ea1418e274dd176d6
CS <rieru89@gmail.com>
2015-10-18 15:45:49 +0800

    Fixed logic to allow for indices following date or float keyword to be delimited by whitespace instead of comma

--- PinkPanther/src/parser/QueryParser.java
+++ PinkPanther/src/parser/QueryParser.java
@@ -15,59 +15,59 @@ public class QueryParser implements Parser {
 	public Pair parse(String commandContent) {
 		String[] userInfo = commandContent.split(",");
 		userInfo = Auxiliary.trimStringArray(userInfo);
+		String[] indexInfo = userInfo[INDEX_INDEX].split(" ");
 		ArrayList<Integer> indexList = new ArrayList<Integer>();
 		SingleDateParser sdp = new SingleDateParser();
 		
+		indexList = generateIndexList(indexInfo);
+		
 		// retrieving multiple floating tasks
 		if (userInfo[INDEX_KEYWORD].equalsIgnoreCase("float")) {
-			if (userInfo.length == 2 && userInfo[INDEX_INDEX].equalsIgnoreCase("all")) {
-				return new Pair<LocalDate, ArrayList<Integer>>(null, null);
+			
+			if (indexInfo.length == 1 && indexInfo[INDEX_KEYWORD].equalsIgnoreCase("all")) {
+				return new Pair<LocalDate, ArrayList<Integer>>(null, indexList);
 			}
 			
-			for (int i = INDEX_INDEX; i < userInfo.length; i++) {
-				if (userInfo[i].isEmpty()) {
-					continue;
-				}
-				
-				try {
-					indexList.add(Integer.valueOf(userInfo[i]));
-				} catch (Exception e) {
-					Display.setFeedBack("Invalid index entered");
-					return null;
-				}
+			if (indexList == null) {
+				Display.setFeedBack("Invalid index entered");
+				return null;
 			}
+			
 			return new Pair<LocalDate, ArrayList<Integer>>(null, indexList);
-			
 		} 
 		
 		// retrieving multiple dated tasks
 		LocalDate date = sdp.parse(userInfo[INDEX_KEYWORD]);
 		if (date != null) {
+			
 			// retrieve all tasks
-			if (userInfo.length == 2 && userInfo[INDEX_INDEX].equalsIgnoreCase("all")) {
-				return new Pair<LocalDate, ArrayList<Integer>>(date, null);
+			if (indexInfo.length == 1 && indexInfo[INDEX_KEYWORD].equalsIgnoreCase("all")) {
+				return new Pair<LocalDate, ArrayList<Integer>>(date, indexList);
 			}
 			// retrieve multiple tasks
-			for (int i = INDEX_INDEX; i < userInfo.length; i++) {
-				if (userInfo[i].isEmpty()) {
-					continue;
-				}
-				try {
-					indexList.add(Integer.valueOf(userInfo[i]));
-				} catch (Exception e) {
-					Display.setFeedBack("Invalid index entered");
-					return null;
-				}
+			if (indexList == null) {
+				Display.setFeedBack("Invalid index entered");
+				return null;
 			}
+			
 			return new Pair<LocalDate, ArrayList<Integer>>(date, indexList);
 		}
 		
 		// no keywords found!
 		Display.setFeedBack("No valid keyword or date entered");
 		return null;
-		
-		
-		
+	}
+	
+	private ArrayList<Integer> generateIndexList(String[] listOfIndices) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = INDEX_KEYWORD; i < listOfIndices.length; i++) {
+			try {
+				list.add(Integer.valueOf(listOfIndices[i]));
+			} catch (Exception e) {
+				return null;
+			}
+		}	
+		return list;
 	}
 }
 
