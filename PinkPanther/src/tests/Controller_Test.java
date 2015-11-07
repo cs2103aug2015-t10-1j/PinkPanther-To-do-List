e8a2b352c2120a818d36dcfdcac31f421aa765c3
gaobo <gaobo@AsusPC>
2015-11-07 16:51:58 +0800

    added junit test for TaskManager and controller

---
+++ PinkPanther/src/tests/Controller_Test.java
@@ -0,0 +1,141 @@
+/* @@author GB */
+
+package tests;
+import logic.Controller;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import common.Display;
+import common.ProgramState;
+
+public class Controller_Test {
+	Controller controller = new Controller();
+	ProgramState state = controller.getProgramState();
+	LocalDate date = LocalDate.of(2015, 11, 7);
+	
+	@Before
+	public void setUp() {
+		controller.addCommand("clear");
+		controller.addCommand("add do homework, 2 to 5pm, 7 nov");
+		controller.addCommand("add feed the dog,6pm,7 nov");
+
+	}
+	
+	@Test
+	public void addTask_positive(){
+
+		controller.addCommand("add revise for exam, 7 to 8pm, 7 nov");
+		String name = state.getTodoList().get(date).get(1).getName();
+		assertEquals(name,"revise for exam");
+		assertEquals(state.getTodoList().get(date).size(),3);
+		String feedbackString = "Task added: " + "[" + "2015-11-07" + "] " + "\"" + "revise for exam" + "\"";
+		assertEquals(Display.showFeedBack(),feedbackString);
+	}
+	
+	@Test
+	public void addTask_negative(){
+		controller.addCommand("add do homework, 2 to 5pm, 7 nov");
+		assertEquals(state.getTodoList().get(date).size(),2);
+		assertEquals(Display.showFeedBack(),"You have already added this task");
+	}
+	
+	@Test
+	public void deleteTask_positive(){
+		controller.addCommand("del 7 nov,1");
+		assertEquals(state.getTodoList().get(date).size(),1);
+		String name = state.getTodoList().get(date).get(0).getName();
+		assertEquals(name,"feed the dog");
+		String feedbackString = "\""+"do homework"+"\""+" has been deleted from "+"2015-11-07";
+		assertEquals(Display.showFeedBack(),feedbackString);
+	}
+	
+	@Test
+	public void deleteTask_negative(){
+		controller.addCommand("del 7 nov,3");
+		assertEquals(state.getTodoList().get(date).size(),2);
+		String feedbackString = "Task or tasks do not exist";
+		assertEquals(Display.showFeedBack(),feedbackString);
+	}
+	
+	@Test
+	public void editTask_positive(){
+		controller.addCommand("edit 7 nov,1");
+		assertEquals(state.getInputBoxText(),"do homework, 2.00PM to 5.00PM, 7/11/15");
+		assertEquals(Display.showFeedBack(),"Edit the task in text box, then press ENTER.");
+	}
+	
+	@Test
+	public void editTask_negative(){
+		controller.addCommand("edit 7 nov,3");
+		assertEquals(state.getInputBoxText(),"edit 7 nov,3");
+		assertEquals(Display.showFeedBack(),"Task or tasks do not exist");
+	}
+	
+	@Test
+	public void taskEdited_postive(){
+		controller.addCommand("edit 7 nov,2");
+		String editedString = "feed the cat,6.00PM,7/11/15";
+		controller.addCommand(editedString);
+		assertEquals(state.getTodoList().get(date).get(1).getName(),"feed the cat");
+		assertEquals(Display.showFeedBack(),"\"feed the dog\" has been modified");
+	}
+	
+	@Test
+	public void taskEdited_negative(){
+		controller.addCommand("edit 7 nov,2");
+		String editedString = "       ";
+		controller.addCommand(editedString);
+		assertEquals(state.getTodoList().get(date).get(1).getName(),"feed the dog");
+		assertEquals(Display.showFeedBack(),"You have not entered a task name");
+	}
+	
+	@Test
+	public void doneTask_positive(){
+		controller.addCommand("done 7 nov,1");
+		assertEquals(state.getTodoList().get(date).size(),1);
+		String feedbackString = "Marked as done: [" + "2015-11-07" + "] " + "\"" + "do homework" + "\"";
+		assertEquals(Display.showFeedBack(),feedbackString);
+		
+		controller.addCommand("view done");
+		assertEquals(state.getTodoList().get(date).get(0).getName(),"do homework");
+	}
+	
+	@Test
+	public void doneTask_negative(){
+		controller.addCommand("done 7 nov,3");
+		assertEquals(state.getInputBoxText(),"done 7 nov,3");
+		assertEquals(Display.showFeedBack(),"Task or tasks do not exist");
+	}
+	
+	@Test
+	public void searchByKeyword_test(){
+		controller.addCommand("search homework");
+		assertEquals(state.getTodoList().get(date).get(0).getName(),"do homework");
+	}
+	
+	@Test
+	public void redo_undo_test(){
+		controller.addCommand("undo");
+		controller.addCommand("undo");
+		assertEquals(state.getTodoList().containsKey(date),false);
+		controller.addCommand("redo");
+		assertEquals(state.getTodoList().get(date).get(0).getName(),"do homework");
+		controller.addCommand("redo");
+		assertEquals(state.getTodoList().get(date).get(1).getName(),"feed the dog");
+	}
+	
+	@Test
+	public void randomWord_test(){
+		controller.addCommand("what is a quantum computer");
+		assertEquals(state.getInputBoxText(),"what is a quantum computer");
+		assertEquals(Display.showFeedBack(),"Unrecognized command. Press F2 for Help Screen.");
+	}
+	
+	
+
+}
---
+++ PinkPanther/src/tests/TaskManager_Test.java
@@ -0,0 +1,144 @@
+/* @@author GB */
+
+package tests;
+import logic.TaskManager;
+import storage.StorageControl;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.SortedMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import common.Display;
+import common.Pair;
+import common.Task;
+import common.TaskType;
+
+public class TaskManager_Test {
+	private StorageControl storage = new StorageControl();
+	private TaskManager manager = new TaskManager(storage);
+	LocalDate date = LocalDate.of(2015, 11, 13);
+	
+	@Before
+	public void setUp() {
+		manager.clearAllTasks();
+		
+		//initializing all tasks
+		
+		Task dated_1 = new Task("write Junit test cases", date,null,TaskType.TODO);
+		Task dated_2 = new Task("submit report", date,LocalTime.of(19, 30),TaskType.DEADLINE);
+		Task dated_3 = new Task("software demo", date,LocalTime.of(17, 0),date,LocalTime.of(18, 0));
+		manager.addTask(dated_1);
+		manager.addTask(dated_2);
+		manager.addTask(dated_3);
+	}
+	
+	@Test
+	public void addTask_isSorted(){
+		Task task1 = new Task("submit developer guide",date,LocalTime.of(18, 30),TaskType.DEADLINE);
+		Task task2 = new Task("apply for scholarship",date,null,TaskType.TODO);
+		manager.addTask(task1);
+		manager.addTask(task2);
+		SortedMap<LocalDate,ArrayList<Task>>todoList = manager.getDated(false);
+		
+		assertEquals(todoList.get(date).size(),5);
+		
+		assertEquals(todoList.get(date).get(3).getName(),"submit developer guide");
+		assertEquals(todoList.get(date).get(4).getName(),"submit report");
+		
+		assertEquals(todoList.get(date).get(1).getName(),"apply for scholarship");
+		assertEquals(todoList.get(date).get(2).getName(),"write Junit test cases");
+		
+		assertEquals(todoList.get(date).get(0).getName(),"software demo");
+		assertEquals(todoList.get(date).get(1).getName(),"apply for scholarship");
+		
+		assertEquals(todoList.get(date).get(2).getName(),"write Junit test cases");
+		assertEquals(todoList.get(date).get(3).getName(),"submit developer guide");
+	}
+	
+	@Test
+	public void addTask_checkDuplicate_negative(){
+		Task task = new Task("watch star war movie",LocalDate.of(2015, 12, 15),null,TaskType.TODO);
+		assertEquals(manager.addTask(task),true);
+	}
+	
+	@Test
+	public void addTask_checkDuplicate_positive(){
+		Task task = new Task("submit report", date,LocalTime.of(19, 30),TaskType.DEADLINE);
+		assertEquals(manager.addTask(task),false);
+	}
+	
+	@Test
+	public void addTask_checkClash_positive(){
+		Task task = new Task("another event", date,LocalTime.of(16, 0),date,LocalTime.of(17, 20));
+		manager.addTask(task);
+		assertEquals(task.getClash(),true);
+		assertEquals(manager.getDated(false).get(date).get(0).getClash(),true);
+	}
+	
+	@Test
+	public void addTask_checkClash_negative(){
+		Task task = new Task("another event", date,LocalTime.of(16, 0),date,LocalTime.of(17, 0));
+		manager.addTask(task);
+		assertEquals(task.getClash(),false);
+		assertEquals(manager.getDated(false).get(date).get(0).getClash(),false);
+	}
+	
+	@Test
+	public void searchTasks_positive(){
+		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1));
+		ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(1,2,3));
+		ArrayList<Integer> list3 = new ArrayList<Integer>(Arrays.asList(1,2,5));
+		Pair<LocalDate,ArrayList<Integer>> pair1 = new Pair<LocalDate,ArrayList<Integer>>(date,list1);
+		Pair<LocalDate,ArrayList<Integer>> pair2 = new Pair<LocalDate,ArrayList<Integer>>(date,list2);
+		Pair<LocalDate,ArrayList<Integer>> pair3 = new Pair<LocalDate,ArrayList<Integer>>(date,list3);
+		
+		
+		ArrayList<Task> output1 = manager.searchTasks(pair1);
+		assertEquals(output1.get(0).getName(),"software demo");
+		
+		ArrayList<Task> output2 = manager.searchTasks(pair2);
+		assertEquals(output2.get(1).getName(),"write Junit test cases");
+		assertEquals(output2.get(2).getName(),"submit report");
+		
+		ArrayList<Task> output3 = manager.searchTasks(pair3);
+		assertEquals(output3.size(),2);
+
+	}
+	
+	@Test
+	public void searchTasks_negative(){
+		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(5));
+		Pair<LocalDate,ArrayList<Integer>> pair1 = new Pair<LocalDate,ArrayList<Integer>>(date,list1);
+		
+		ArrayList<Task> output1 = manager.searchTasks(pair1);
+		assertEquals(output1,null);
+		assertEquals(Display.showFeedBack(),"Task or tasks do not exist");
+	}
+	
+	@Test
+	public void deleteTask_taskDeleted(){
+		Task task = manager.searchTasks(new Pair<LocalDate,ArrayList<Integer>>(date,
+				new ArrayList<Integer>(Arrays.asList(2)))).get(0);
+		manager.deleteTask(task);
+		assertEquals(manager.getDated(false).get(date).get(1).getName(),"submit report");
+		assertEquals(manager.getDated(false).get(date).size(),2);
+	}
+	
+	@Test
+	public void deleteTask_keyDeleted(){
+		LocalDate date1 = LocalDate.of(2015, 11, 10);
+		Task task = new Task("random event",date1,null,date1,null);
+		manager.addTask(task);
+		assertEquals(manager.getDated(false).containsKey(date1),true);
+		manager.deleteTask(task);
+		assertEquals(manager.getDated(false).containsKey(date1),false);
+	}
+
+}
